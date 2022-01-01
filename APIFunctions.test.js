28273b8a21c0cf10dee9e6ecc08e2adf27fb10ee
Matilda Mared <matildamared@live.se>
2022-01-01 21:48:23 +0100

    move mock to its own directory

--- APIFunctions.test.js
+++ APIFunctions.test.js
@@ -1,71 +1,22 @@
 const { beforeEach } = require("@jest/globals");
 const { getProducts, getById } = require("./APIfunctions");
 const collection = require("./collection");
-
-const products = [
-	{
-		id: "1",
-		name: "Fotboll",
-		details: "En boll man kan sparka på",
-		price: 120,
-		inStock: 20,
-		image: "https://jkashdkasjdja.jpg",
-	},
-	{
-		id: "2",
-		name: "Basketboll",
-		details: "En boll att kasta i en korg",
-		price: 310,
-		inStock: 15,
-		image: "https://hjkdfksjdf.se",
-	},
-	{
-		id: "3",
-		name: "Krocketspel",
-		details: "Ett härligt spel att spela på sommaren",
-		price: 200,
-		inStock: 5,
-		image: "https://fsdkjfghskdfjghs.se",
-	},
-	{
-		id: "4",
-		name: "Innebandyklubba",
-		details: "Den bästa klubban för dig och ditt bandylag",
-		price: 1500,
-		inStock: 3,
-		image: "https://ghs.se",
-	},
-	{
-		id: "5",
-		name: "Pingisrack",
-		details: "Ska du bli bäst på pingis? Då är detta racket för dig.",
-		price: 150,
-		inStock: 8,
-		image: "https://dfsdfdjghs.se",
-	},
-];
-
-jest.mock("./collection", () => () => ({
-	getById: () => "mock result",
-	filter: (filterString) => {
-		return products.filter((product) => {
-			return product.name.includes(filterString);
-		});
-	},
-}));
+jest.mock("./collection");
 
 describe("getProducts", () => {
 	it("returns a list of products that match the name", async () => {
 		const testSearchString = "Fotboll";
 
-		const expectedResult = {
-			id: "1",
-			name: "Fotboll",
-			details: "En boll man kan sparka på",
-			price: 120,
-			inStock: 20,
-			image: "https://jkashdkasjdja.jpg",
-		};
+		const expectedResult = [
+			{
+				id: "1",
+				name: "Fotboll",
+				details: "En boll man kan sparka på",
+				price: 120,
+				inStock: 20,
+				image: "https://jkashdkasjdja.jpg",
+			},
+		];
 
 		const actualResult = await getProducts(testSearchString);
 
---
+++ __mocks__/collection.js
@@ -0,0 +1,51 @@
+const products = [
+	{
+		id: "1",
+		name: "Fotboll",
+		details: "En boll man kan sparka på",
+		price: 120,
+		inStock: 20,
+		image: "https://jkashdkasjdja.jpg",
+	},
+	{
+		id: "2",
+		name: "Basketboll",
+		details: "En boll att kasta i en korg",
+		price: 310,
+		inStock: 15,
+		image: "https://hjkdfksjdf.se",
+	},
+	{
+		id: "3",
+		name: "Krocketspel",
+		details: "Ett härligt spel att spela på sommaren",
+		price: 200,
+		inStock: 5,
+		image: "https://fsdkjfghskdfjghs.se",
+	},
+	{
+		id: "4",
+		name: "Innebandyklubba",
+		details: "Den bästa klubban för dig och ditt bandylag",
+		price: 1500,
+		inStock: 3,
+		image: "https://ghs.se",
+	},
+	{
+		id: "5",
+		name: "Pingisrack",
+		details: "Ska du bli bäst på pingis? Då är detta racket för dig.",
+		price: 150,
+		inStock: 8,
+		image: "https://dfsdfdjghs.se",
+	},
+];
+
+module.exports = () => ({
+	getById: () => "mock result",
+	filter: (filterString) => {
+		return products.filter((product) => {
+			return product.name.includes(filterString);
+		});
+	},
+});
